67d01a65387ea4db8d8dc30db79ccabdbc529f21
Wellington Felix <wfmf@cin.ufpe.br>
2018-12-02 12:35:06 -0300

    Adiciona CRUD de Card

--- app/model/Card.js
+++ app/model/Card.js
@@ -9,7 +9,7 @@ const Schema = mongoose.Schema;
 const Card = new Schema({
     token: { type: String, unique: true },
     text: { type: String, default: '' },
-    userDeviceId: { type: String, unique: true, default: null },
+    // userDeviceId: { type: String, unique: true, default: null },
     section: { type: Schema.Types.ObjectId, ref: 'Section', default: null }
 },
 {
--- app/routes/cardRouter.js
+++ app/routes/cardRouter.js
@@ -1,11 +1,72 @@
 const { Router } = require('express');
-
+const Card = require('../model/Card');
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.json({
-        // test: 'SHOW DE TOP'
-    });
+    try {
+        const cards = await Card.find();
+        res.json(cards);
+    } catch(e) {
+        console.log(e);
+        res.status(500).json({
+            error: 'Error while getting card list'
+        });
+    }
+});
+
+router.get('/:id', (req, res) => {
+    try {
+        const card = await Card.findById(req.params.id);
+        res.json(card);
+    } catch(e) {
+        console.log(e);
+        res.status(500).json({
+            error: 'Error while getting card info'
+        });
+    }
+});
+
+router.post('/', (req, res) => {
+    try {
+        const { token, text } = req.body;
+        const card = new Card({
+            token,
+            text
+        });
+        res.json(card);
+    } catch(e) {
+        console.log(e);
+        res.json({
+            error: 'Error while creating card'
+        });
+    }
+});
+
+router.put('/:id', (req, res) => {
+    try {
+        const card = Card.findByIdAndUpdate(req.params.id, {
+            section: req.body.section
+        },
+        {new: true});
+        res.json(card);
+    } catch(e) {
+        console.log(e);
+        res.json({
+            error: 'Error while updating card'
+        });
+    }
+});
+
+router.delete('/:id', (req, res) => {
+    try {
+        const card = Card.findByIdAndRemove(req.params.id);
+        res.json(card);
+    } catch(e) {
+        console.log(e);
+        res.json({
+            error: 'Error while deleting card'
+        });
+    }
 });
 
 module.exports = router;
